0f57b8c6506aa4f5e9680827870070989f631695
lijx60 <fjsmlijx@126.com>
2020-12-11 08:46:45 +0800

    first sent websever

---
+++ websever1.ino
@@ -0,0 +1,57 @@
+//利用NODEMCU，建立基本网络服务器
+#include <ESP8266WiFi.h>        // 程序调用 ESP8266WiFi库
+#include <ESP8266WiFiMulti.h>   // 为多个wifi，调用 ESP8266WiFiMulti库
+#include <ESP8266WebServer.h>   // 调用 ESP8266WebServer库
+ 
+ESP8266WiFiMulti wifiMulti;     // 建立ESP8266WiFiMulti对象,适应多个wifi.对象名称是'wifiMulti'
+ 
+ESP8266WebServer esp8266_server(80);// 建立ESP8266WebServer对象，对象名称为esp8266_server
+                                    // 括号中的数字是网路服务器响应http请求的端口号
+                                    // 网络服务器标准http端口号为80，因此这里使用80为端口号
+ 
+void setup(void){
+  Serial.begin(9600);          // 启动串口通讯
+ 
+  //通过addAp函数存储  WiFi名称       WiFi密码
+  wifiMulti.addAP("xiaomi804", "ljx365000");  // 这三条语句通过调用函数addAP来记录3个不同的WiFi网络信息。
+  wifiMulti.addAP("xiaomen", "88888888"); // 这3个WiFi网络名称分别是xiaomen, xiaomen2, xiaomen3。
+  wifiMulti.addAP("xiaomen1", "66666666"); // 这3个网络的密码分别是ljx365000，88888888，66666666。
+                                                // 此处WiFi信息只是示例，请在使用时将需要连接的WiFi信息填入相应位置。
+                                                // 另外这里只存储了3个WiFi信息，您可以存储更多的WiFi信息在此处。
+ 
+  int i = 0;                                 
+  while (wifiMulti.run() != WL_CONNECTED) {  // 此处的wifiMulti.run()是重点。通过wifiMulti.run()，NodeMCU将会在当前
+    delay(1000);                             // 环境中搜索addAP函数所存储的WiFi。如果搜到多个存储的WiFi那么NodeMCU
+    Serial.print(i++); Serial.print(' ');    // 将会连接信号最强的那一个WiFi信号。
+  }                                          // 一旦连接WiFI成功，wifiMulti.run()将会返回“WL_CONNECTED”。这也是
+                                             // 此处while循环判断是否跳出循环的条件。
+ 
+  // WiFi连接成功后将通过串口监视器输出连接成功信息 
+  Serial.println('\n');                     // WiFi连接成功后
+  Serial.print("Connected to ");            // NodeMCU将通过串口监视器输出。
+  Serial.println(WiFi.SSID());              // 连接的WiFI名称
+  Serial.print("IP address:\t");            // 以及
+  Serial.println(WiFi.localIP());           // NodeMCU的IP地址
+  
+//根据库函数，调用三个函数，启动网络服务功能 程序重点。// 
+  esp8266_server.begin();                   //  开启网络服务器，通过80端口，开始提供网站服务
+  esp8266_server.on("/", handleRoot);       //  浏览器想要访问/（首页）时，调用handleRoot处理
+  esp8266_server.onNotFound(handleNotFound);//  浏览器想要访问页面不存在时，回复404      
+//--------"启动网络服务功能"程序部分结束--------
+  Serial.println("HTTP esp8266_server started");//  告知用户ESP8266网络服务功能已经启动
+}
+ 
+
+void loop(void){
+  esp8266_server.handleClient();     // 处理http服务器访问
+}
+ 
+                                                                         
+void handleRoot() {   //处理网站根目录“/”的访问请求 
+  esp8266_server.send(200, "text/plain", "Hello world,I am from ESP8266");   // NodeMCU将调用此函数。
+}
+ 
+// 设置处理404情况的函数'handleNotFound'
+void handleNotFound(){                                        // 当浏览器请求的网络资源无法在服务器找到时，
+  esp8266_server.send(404, "text/plain", "404: Not found");   // NodeMCU将调用此函数。
+}
